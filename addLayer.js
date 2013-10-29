6a9f0d5a250246e01d2f91f62ad8bb85d4c38556
mahengyang <mahengyang163@163.com>
2013-10-29 23:45:19 +0800

    add some link for google.com.hk

--- addLayer.js
+++ addLayer.js
@@ -1,1 +1,9 @@
-console.log('this is from addLayer.js')
+console.log('this is from addLayer.js');
+$('#hplogo').children().text("enyo");
+var newdiv = "<div class='linkweb'></div>";
+$(document.body).append(newdiv)
+$('.linkweb').append("<a href='http://www.jd.com'>京东</a><p />")
+$('.linkweb').append("<a href='http://www.taobao.com'>淘宝</a><p />")
+$('.linkweb').append("<a href='http://www.ifanr.com'>爱范儿</a><p />")
+$('.linkweb').append("<a href='http://www.zhihu.com'>知乎</a><p />")
+$('.linkweb').append("<a href='http://www.appinn.com'>小众软件</a><p />")
