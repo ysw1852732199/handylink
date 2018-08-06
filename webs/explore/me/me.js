7f94df2fd901c3c88eb32b1ffbcf4e0c0a1ce6da
Yang Chun <825660677@qq.com>
2018-08-06 17:36:10 +0800

    完善个人资料

--- webs/explore/me/me.js
+++ webs/explore/me/me.js
@@ -6,7 +6,38 @@ define([
       name: "me",
       template: Template,
       data: function() {
-        return {}
+        return {
+          edus: {
+            titles: [{
+              title: "时间",
+              data: "time",
+              width: "200px"
+            },{
+              title: "学历",
+              data: "level",
+              width: "100px"
+            },{
+              title: "学校",
+              data: "university",
+              width: "160px"
+            },{
+              title: "专业",
+              data: "major",
+              width: "160px"
+            },{
+              title: "主修课程",
+              data: "courses",
+              width: ""
+            }],
+            data: [{
+              time: "2007年09月~2011年07月",
+              level: "本科",
+              university: "河南农业大学",
+              major: "计算机科学与技术",
+              courses: "大学数学，线性代数，计算机组成原理，计算机网络，C语言程序设计"
+            }]
+          }
+        }
       }
     }
   });
