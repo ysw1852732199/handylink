4f9e420d56e6abf8484e1fb4d7be64be88c5e7cd
Yang Chun <825660677@qq.com>
2018-09-03 17:00:13 +0800

    添加map area练习示例

---
+++ webs/explore/area/area.js
@@ -0,0 +1,10 @@
+define(["text!./area.html"], function(htmlTemplate) {
+    'use strict';
+    return {
+      name: "mapArea",
+      template: htmlTemplate,
+      data: function() {
+        return {}
+      }
+    }
+  });
--- webs/explore/router.js
+++ webs/explore/router.js
@@ -3,27 +3,32 @@ define(["explore/explore",
     "./todo/todo",
     "./me/me",
     "./alipay/alipay",
-    "./weixin/pay"], function(Explore, Todo, AboutMe, AliPay, WeixinPay) {
+    "./weixin/pay",
+    "./area/area"], function(Explore, Todo, AboutMe, AliPay, WeixinPay, Area) {
     return {
-            path: "/explore",
-            name: "explore",
-            component: Explore,
-            children: [{
-                path: "",
-                name: "explore_about_me",
-                component: AboutMe
-            },{
-                path: "todo",
-                name: "explore_todo",
-                component: Todo
-            },{
-                path: "alipay",
-                name: "explore_alipay",
-                component: AliPay
-            },{
-                path: "wxpay",
-                name: "explore_wxpay",
-                component: WeixinPay
-            }]
-        };
+        path: "/explore",
+        name: "explore",
+        component: Explore,
+        children: [{
+            path: "",
+            name: "explore_about_me",
+            component: AboutMe
+        },{
+            path: "todo",
+            name: "explore_todo",
+            component: Todo
+        },{
+            path: "alipay",
+            name: "explore_alipay",
+            component: AliPay
+        },{
+            path: "wxpay",
+            name: "explore_wxpay",
+            component: WeixinPay
+        },{
+            path: "area",
+            name: "explore_area",
+            component: Area
+        }]
+    };
 });
